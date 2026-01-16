4636de5f0a9572adef1f460517fabdb853d29380
brendon <brendon.lopes.21@gmail.com>
2026-01-16 19:14:48 -0300

    feat: initialize event-loop project with main files and dependencies

---
+++ event-loop/index.js
@@ -0,0 +1,37 @@
+import fs from 'node:fs';
+
+console.log('1. Start');
+
+setTimeout(() => {
+  // non-deterministic order with immediate
+  console.log('5.? Timeout');
+}, 0);
+
+Promise.resolve().then(() => {
+  console.log('3.1 Promise 1');
+
+  Promise.resolve().then(() => {
+    console.log('3.3 Promise 3');
+  });
+});
+
+process.nextTick(() => {
+  console.log('4. Next Tick');
+});
+
+fs.readFile('./file.txt', 'utf-8', (err, data) => {
+  setImmediate(() => {
+    console.log('6. Immediate from I/O');
+  });
+});
+
+setImmediate(() => {
+  // non-deterministic order with timeout
+  console.log('5.? Immediate');
+});
+
+Promise.resolve().then(() => {
+  console.log('3.2 Promise 2');
+});
+
+console.log('2. End');
